5a119ada43b9960967338e83065935e1b48d731b
Joshua Book <joshua.book@gmail.com>
2016-04-10 16:14:50 -0400

    move reducer into module and add mocha to test function

---
+++ app/reducers/index.js
@@ -0,0 +1,18 @@
+export function todos (state = [], action) {
+  switch(action.type) {
+    case('ADD_TODO'):
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ]
+    default:
+      return state;
+  }
+
+  return state;
+};
+
