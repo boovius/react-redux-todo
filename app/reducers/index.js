d3ddf0ccaf3db964eaee20d0eef55dfcb72f9ccf
Joshua Book <joshua.book@gmail.com>
2016-04-12 13:48:57 -0400

    add toggle_todo action and test to reducer and factor out todo reducer using reducer composition

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,18 +1,35 @@
+const todo = (state = {}, action) => {
+  switch(action.type) {
+    case('ADD_TODO'):
+      return {
+        ...state,
+        text: action.text,
+        completed: false
+      }
+    case('TOGGLE_TODO'):
+      return {
+        ...state,
+        completed: !state.completed
+      }
+  }
+}
+
 export function todos (state = [], action) {
   switch(action.type) {
     case('ADD_TODO'):
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
+        todo({id: state.length}, action)
+      ]
+    case('TOGGLE_TODO'):
+      return [
+        ...state.slice(0, action.id),
+        todo(state[action.id], action),
+        ...state.slice([action.id+1])
       ]
     default:
       return state;
   }
-
-  return state;
 };
+
 
--- test/test.js
+++ test/test.js
@@ -3,26 +3,75 @@ import expect from 'expect';
 import { todos } from '../app/reducers/index';
 
 describe('todos', () => {
+  describe('ADD_TODO', () => {
+    const stateBefore = [];
+    const action = {
+      type: 'ADD_TODO',
+      text: 'Learn Redux'
+    };
 
-  const stateBefore = [];
-  const action = {
-    type: 'ADD_TODO',
-    id: 0,
-    text: 'Learn Redux'
-  };
+    const stateAfter = [
+      {
+        id: 0,
+        text: 'Learn Redux',
+        completed: false
+      }
+    ];
 
-  const stateAfter = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false
-    }
-  ];
+    deepFreeze(stateBefore);
+    deepFreeze(action);
 
-  deepFreeze(stateBefore);
-  deepFreeze(action);
+    it('adds a todo and does not mutate original state', () => {
+      expect(todos(stateBefore, action)).toEqual(stateAfter);
+    });
+  });
 
-  it('adds a todo and does not mutate original state', () => {
-    expect(todos(stateBefore, action)).toEqual(stateAfter);
+  describe('TOGGLE_TODO', () => {
+    const stateBefore = [
+      {
+        id: 0,
+        text: 'Learn Relay',
+        completed: false
+      },
+      {
+        id: 1,
+        text: 'Learn Redux',
+        completed: false
+      },
+      {
+        id: 1,
+        text: 'Learn Velocity',
+        completed: false
+      }
+    ];
+    const action = {
+      type: 'TOGGLE_TODO',
+      id: 1,
+    };
+
+    const stateAfter = [
+      {
+        id: 0,
+        text: 'Learn Relay',
+        completed: false
+      },
+      {
+        id: 1,
+        text: 'Learn Redux',
+        completed: true
+      },
+      {
+        id: 1,
+        text: 'Learn Velocity',
+        completed: false
+      }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    it('toggles the completed field on the todo', () => {
+      expect(todos(stateBefore, action)).toEqual(stateAfter);
+    });
   });
 });
