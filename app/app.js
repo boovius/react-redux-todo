9f5e0fa27e431c825109dcb65aa888e1acf1d464
Joshua Book <joshua.book@gmail.com>
2016-04-14 10:45:52 -0400

    add non-working todoapp component

--- app/app.js
+++ app/app.js
@@ -1,29 +1,45 @@
 import { createStore } from 'redux';
-
 import { todoApp } from './reducers';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 let store = createStore(todoApp);
 
-console.log(store.getState());
+let nextTodoId = 0;
 
-store.dispatch({
-  type: 'ADD_TODO',
-  text: 'learn redux'
-});
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            text: 'Test',
+            id: nextTodoId++
+          })
+        }}
+        >
+          Add Todo
+        </button>
+        <ul>
+          {this.props.todos}.map(todo =>
+            <li key={todo.id}>
+              {todo.text}
+            </li>
+          )
+        </ul>
+      </div>
+    )
+  }
+}
 
-console.log(store.getState());
+const render = () => {
+  ReactDOM.render(
+    <TodoApp todos={store.getState().todos}/>,
+    document.getElementById('root')
+  )
+}
 
-store.dispatch({
-  type: 'TOGGLE_TODO',
-  id: 0
-});
+store.subscribe(render);
+render();
 
-console.log(store.getState());
-
-
-store.dispatch({
-  type: 'SET_VISIBILITY_FILTER',
-  filter: 'HIDE_COMPLETED'
-});
-
-console.log(store.getState());
